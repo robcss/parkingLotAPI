f0daf1f1105a35091a4a9361f9f373e94fa89d49
robcass <77405130+robcss@users.noreply.github.com>
2021-07-08 20:33:49 +0200

    add basic info and park routes

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
 const parkingLot = require("./parkingLot")
 
-console.log(parkingLot)
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+
+app.get("/info", (req, res) => {
+
+    const info = {
+        size: parkingLot.length,
+        emptySlots: parkingLot.filter(slot => slot === null).length
+    }
+
+    res.json(info)
+})
+
+
+app.post("/park", (req, res) => {
+    const { licensePlate } = req.body
+
+    const emptySlot = parkingLot.indexOf(null)
+
+    if (emptySlot === -1) {
+        res.json({ error: "Parking lot is full!" })
+        return;
+    }
+
+    parkingLot[emptySlot] = licensePlate
+
+    console.log(parkingLot)
+
+    const parked = {
+        licensePlate,
+        slot: emptySlot
+    }
+
+    res.json(parked)
+})
+
+
+const port = 3000
+
+app.listen(port, () => console.log(`Listening on port ${port}`))
